368bf08d1bc5ef000edaa653a82f7be7d8a2deb8
Marcos G. Quiles <quiles@gmail.com>
2022-07-13 17:01:31 -0300

    Aula11 v2

--- Aula11/ex03.c
+++ Aula11/ex03.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX 100
+
 typedef struct {
 	char nome[100];
 	char RA[15];
 	float nota;
 	int freq;
 } TAluno;
+
+TAluno alunos[MAX];
+
 
 int menu(){
 	int op;
@@ -20,23 +25,67 @@ int menu(){
 	return op;
 }
 
+int Cadastra_Aluno(int index){
+	fflush(stdin);
+	if (index < MAX) {
+		printf("Nome: ");
+		fgets(alunos[index].nome, 100, stdin);
+		alunos[index].nome[strlen(alunos[index].nome)-1] = '\0';
+		printf("RA: ");
+		fgets(alunos[index].RA, 15, stdin);
+		alunos[index].RA[strlen(alunos[index].RA)-1] = '\0';
+		printf("Nota: ");
+		scanf("%f", &alunos[index].nota);
+		printf("Frequencia: ");
+		scanf("%d", &alunos[index].freq);
+		return index+1;
+	}
+	else { 
+		printf("\n\nLista cheia\n\n");
+		return index;
+	}
+}
+
+
+void Consulta_Aluno(int index){
+	int idx;
+	printf("Qual aluno? ");
+	scanf("%d", &idx);
+	if (idx >= index) printf("\n\nAluno não cadastrado \n\n");
+	else {
+		printf("Dados do aluno %d\n", idx);
+		printf("Nome: %s\n", alunos[idx].nome);
+		printf("RA: %s\n", alunos[idx].RA);
+		printf("Nota: %.1f\n", alunos[idx].nota);
+		printf("Frequencia: %d\n\n\n", alunos[idx].freq);
+	}
+}
+
+void Imprime_Rel(int);
 
 int main(){
-	TAluno alunos[100];
-	int op;
-	
+	int op, res;
+	int index = 0;
 	do {
 		op = menu();
-
 		switch(op){
-			case 1: Cadastra_Aluno(); break;
-			case 2: Consulta_Aluno(); break;
-			case 3: Imprime_Rel(); break;
+			case 1: index = Cadastra_Aluno(index); break;
+			case 2: Consulta_Aluno(index); break;
+			case 3: Imprime_Rel(index); break;
 		}
-
 	} while (op != 9);
 	
 	return 0;
+}
+
+void Imprime_Rel(int index){
+	for (int i=0 ; i<index ; i++){
+		printf("Aluno %d\n", i+1);
+		printf("Nome: %s\n",alunos[i].nome);
+		printf("RA: %s\n", alunos[i].RA);
+		printf("Nota: %.1f\n", alunos[i].nota);
+		printf("Frequencia: %d\n", alunos[i].freq);
+	}
 }
 
 
