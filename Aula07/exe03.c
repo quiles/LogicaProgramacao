e67354be238a34b7ead6144aa65adc8297ea923e
quiles <quiles@CIDAG-XPS01>
2024-04-12 16:30:37 -0300

    Add: aula 7 sort

---
+++ Aula07/exe03.c
@@ -0,0 +1,43 @@
+/*********************************
+ *  Lógica de Programação
+ * 	Linguagem C
+ *  Prof. Dr. Marcos G. Quiles
+ ********************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 20
+
+int main(){
+    int vetor[20], i, j, temp, troca, n=20;
+    srand(time(0));
+    // gerando o vetor com 20 numeros
+    for (i=0 ; i<N ; i++) {
+        vetor[i] = rand()%100;
+        printf("%d ", vetor[i]);
+    }
+
+    for (i=0; i<N-1; i++) {
+        troca = 0; // Flag para otimizar o algoritmo
+        for (j = 0; j<N-i-1; j++) {
+            if (vetor[j] > vetor[j+1]) {
+                // Troca os elementos de posição
+                temp = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = temp;
+                troca = 1; // Uma troca foi realizada
+            }
+        }
+        if (troca == 0) {
+            break;
+        }
+    }
+
+    printf("\n\nVetor Ordenado: \n");
+    for (i=0 ; i<N ; i++) {
+        printf("%d ", vetor[i]);
+    }
+
+    return 0;
+}
